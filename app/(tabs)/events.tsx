2dafe7f6c71ed8d18d17390926ca9db77463fdd5
Alfred <alfred@stensatter.se>
2025-04-05 11:28:07 +0200

    added static events page

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,137 +1,59 @@
 import React, { useEffect, useCallback, useState, useRef } from 'react';
-import { StyleSheet, Dimensions, ScrollView, TouchableOpacity} from 'react-native';
+import { StyleSheet, Dimensions, ScrollView, TouchableOpacity, ImageBackground } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 import Icon from "react-native-vector-icons/FontAwesome"; // Install if not already
 import { useFocusEffect } from '@react-navigation/native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import API from "@/components/api";
+import API, { getProfile } from "@/components/api";
 
 import axios from "axios"
 
 import {Chip,Colors, Spacings, Image, SegmentedControl, Text, Button, Assets, Modal, Card, View} from "react-native-ui-lib"
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 
 const { height, width } = Dimensions.get('window');
 
-const RoundButton = ({ onPress, iconName }) => {
-  return (
-      <TouchableOpacity style={{
-		  width: 50,
-		  height: 50,
-		  borderRadius: 25, // Makes it round
-		  //backgroundColor: "#007AFF", // Change color as needed
-		  justifyContent: "center",
-		  alignItems: "center",
-		  elevation: 5, // Adds shadow on Android
-		  shadowColor: "#000",
-		  shadowOffset: { width: 0, height: 2 },
-		  shadowOpacity: 0.3,
-		  shadowRadius: 2,
-	  }} onPress={onPress}>
-		  <Icon name={iconName} size={64} color={Colors.primary} />
-      </TouchableOpacity>
-  );
-};
-
-
-const bookmark = (userId=>{
-	API.post("/bookmark/",{'userId':userId}).then(response=>{
-		console.log(response.data)
-	}).catch(error=>{
-		console.log(error)
-	})
-})
-
-
-const ProfileCard = ({ card, open }) => {
-
-	if(!card) {return (<Text>Loading</Text>)}
-	else {
-		return (
-			<ScrollView >
-				<View style={{width:"100%", height:height-150}}>
-					<Image
-						style={{ flex: 1, width: '100%',borderRadius: 100 }}
-									resizeMode="cover"
-									source={{uri: `http://192.168.1.119:8000/${card.profile_picture}`}}/>
-					<View style={{position:"absolute", bottom:15, left:15,width:"100%"}}  >
-						<View style={{}}>
-							<Text white text40 >
-								{card.username}
-							</Text>
-							<Chip
-								resetSpacings
-								label={card.pronoun}
-											labelStyle={{marginRight: Spacings.s1}}
-											backgroundColor={"white"}
-											containerStyle={{
-												borderWidth: 0,
-												width:80
-												//marginLeft: Spacings.s3
-											}}/>
-							<Text white text90>
-								{card.programe}
-							</Text>
-							<Text white text90>
-								{card.swipes}
-							</Text>
-
-						</View>
-						<View style={{flex:1, flexDirection:'row', justifyContent:"end", width:"100%",marginTop:15, paddingRight:15}}>
-							<RoundButton iconName="heart" onPress={()=> bookmark(card.id)} />
-							{/* <Link href={`profileswipe?userId=${card.id}`}>
-								<RoundButton iconName="info" />
-								</Link> */}
-						</View>
-					</View>
-				</View>
-
-			</ScrollView>
-		);
-	}
+const NewEvent = ({small}) =>{
+	return(
+		<ImageBackground
+			source={{uri:'http://192.168.1.119:8000/media/uploads/Erik-XIV-toppbild.jpg'}}
+				   style={{
+					   margin:10,
+					   width:  small? 120 : "100%",
+					   height: small? 120 : height*0.2,
+					   borderRadius: 20,
+					   overflow: 'hidden', // 🔥 this is key
+					   justifyContent: 'flex-end',
+				   }}
+				   resizeMode="cover" // or "contain", "stretch", etc.
+		>
+			<View style={{ padding: 10,  borderRadius: 100 }}>
+				<Text white style={{fontSize:12}}>Enginering dinner</Text>
+			</View>
+		</ImageBackground>
+	)
 }
 
-function swiped(profile:any) {
-	console.log(profile)
 
-	API.post("/swipe/",{
-		"id": profile.id,
-		"direction":"right"
-	}).then(response=>{
-		console.log(response)
-	}).catch(error=>{
-
-	})
-
-}
-
-const SwipeScreen = () => {
+const EventScreen = () => {
 
 	const [profiles, setProfile] = useState([]);
-	const [value, setValue] = React.useState('sverige');
-	const [visible, setVisible] = useState(false)
+	const [school,setSchool] = useState<number>(0);
 
+	const [user,setUser] = useState(null);
 	useFocusEffect(useCallback(()=>{
 
-		API.get('/get_swipes')
-			 .then(function (response) {
-				 // handle success
-				 setProfile(response.data.concat(response.data));
-			 })
-			 .catch(function (error) {
-				 // handle error
-				 console.log(error);
-			 })
-			 .finally(function () {
-				 // always executed
-			 });
-
+		getProfile().then(response=>{
+				setUser(response);
+			}).catch(error=>{
+				router.push("/login", { relativeToDirectory: true })
+			});
 	},[]))
 
 	return (
-		<SafeAreaView style={{flex:1}} >
+		<SafeAreaView style={{flex:1,backgroundColor:"#000",}} >
 
 			<View
 				style={{
@@ -143,27 +65,47 @@ const SwipeScreen = () => {
 				}}
 			>
 				<SegmentedControl
-					segments={[{ label: 'Sverige' }, { label: 'Lunds Universited' }]}
-					activeBackgroundColor={Colors.white}
-					activeColor={Colors.primary}
-					backgroundColor={Colors.primary}
+					segments={[{ label: 'Sverige' }, { label: user?.school || "" }]}
+					activeBackgroundColor={Colors.primary}
+					activeColor={Colors.white}
+					backgroundColor={"#010101aa"}
+					onChangeIndex={setSchool}
 					inactiveColor={Colors.white}
-					style={{height:50,width:"90%"}}
 				/>
 			</View>
 
+			<View
+				style={{
+					marginTop:50,
+					padding:20,
+					height:height-90,
+				}} >
+				<Text white heading2 marginB-10>
+					Nya Event
+				</Text>
+				<ScrollView horizontal  >
+					<NewEvent small>
+					</NewEvent>
+					<NewEvent small>
+					</NewEvent>
+					<NewEvent small>
+					</NewEvent>
+				</ScrollView>
 
-			{/* Swiper should take the rest of the space */}
-			<View style={{ flex: 1, backgroundColor: "pink" }}>
-				<Swiper
-					cards={profiles}
-					renderCard={(card) => <ProfileCard card={card} open={()=>setVisible(true)} />}
-					stackSize={3}
-					verticalSwipe={false}
-					onSwipedRight={e=>swiped(profiles[e])}
-					containerStyle={{ marginTop: 0, backgroundColor:Colors.$backgroundDefault }}
-				/>
+				<Text white heading2 marginV-10>
+					Populära Event
+				</Text>
+				<ScrollView  >
+					<NewEvent></NewEvent>
+					<NewEvent></NewEvent>
+					<NewEvent></NewEvent>
+					<View style={{ height: 100 }} />
+				</ScrollView>
+
+
 			</View>
+
+
 
 		</SafeAreaView>
 
@@ -171,4 +113,4 @@ const SwipeScreen = () => {
 };
 
 
-export default SwipeScreen;
+export default EventScreen;
